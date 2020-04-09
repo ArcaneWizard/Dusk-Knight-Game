fb23e48994071a943307bbd7839999de7862c1a9
Foo Bonjour <osc245@gmail.com>
2020-04-09 11:35:50 +0000

    No Comments

---
+++ Assets/Scripts/grenade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenade : MonoBehaviour
+{
+    public float speed;
+    public float fuse;
+
+    void Start()
+    {
+        Invoke("DestroyGrenade", fuse);
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+        
+    }
+
+    void DestroyGrenade()
+    {
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+
+    public GameObject grenade;
+    public Transform muzzle;
+    private float cooldown;
+    private float startcooldown;
+
+    void Start()
+    {
+        startcooldown = 0.3f;
+        cooldown = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
+            float rot = Mathf.Atan2(touchPosition.y, touchPosition.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot);
+            if (cooldown <= 0)
+            {
+                cooldown = startcooldown;
+                Fire(rot);
+            }
+            else
+            {
+                cooldown -= Time.deltaTime;
+            }
+            
+        }
+        cooldown -= Time.deltaTime;
+    }
+
+    void Fire(float rot)
+    {
+        Instantiate(grenade, muzzle.position, Quaternion.Euler(0f, 0f, rot - 30));
+    }
+}
