9901fe6ebfd59d481cdcc4c4b83e4f373a49671f
Foo Bonjour <osc245@gmail.com>
2020-04-11 01:57:08 +0000

    No Comments

--- Assets/Scripts/grenade.cs
+++ Assets/Scripts/grenade.cs
@@ -25,7 +25,7 @@ public class grenade : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.parent.name == "Enemies")
         {
