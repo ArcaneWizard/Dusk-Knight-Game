32eeb84778d062811d664021c923f51717c2deee
Foo Bonjour <osc245@gmail.com>
2020-04-21 06:03:47 +0000

    No Comments

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -8,6 +8,11 @@ public class Health : MonoBehaviour
     public static int playerHP;
     public static int maxPlayerHP;
 
+
+
+    public BoxCollider2D boxy;
+    public BoxCollider2D boxy2;
+    public int stage = 1;
     public static int OrcDmg = 20;
     public static int GobDmg = 15;
     public static int R2Dmg = 15;
@@ -27,6 +32,9 @@ public class Health : MonoBehaviour
     void Update()
     {
         hp.fillAmount = (float) playerHP / maxPlayerHP;
+
+        Debug.Log(maxPlayerHP);
+        Debug.Log(stage);
     }
 
     void OnCollisionEnter2D (Collision2D col)
@@ -43,4 +51,58 @@ public class Health : MonoBehaviour
                 playerHP -= 25;
         }
     }
+
+    public void Reset()
+    {
+        maxPlayerHP = 700;
+        playerHP = maxPlayerHP;
+        boxy.offset = new Vector2(0.02191818f, -0.5f);
+        boxy.size = new Vector2(2.344175f, 1.9f);
+        boxy2.offset = new Vector2(0.03646278f, 0.9f);
+        boxy2.size = new Vector2(2.458501f, 0.7449135f);
+        transform.GetChild(2).GetChild(5).gameObject.SetActive(false);
+        transform.GetChild(2).GetChild(6).gameObject.SetActive(false);
+        transform.GetChild(2).GetChild(7).gameObject.SetActive(false);
+        transform.GetChild(2).GetChild(3).gameObject.SetActive(false);
+        transform.GetChild(2).GetChild(4).gameObject.SetActive(false);
+        transform.GetChild(2).GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(2).GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(2).GetChild(2).gameObject.SetActive(true);
+    }
+
+    public void Upgrade()
+    {
+        stage++;
+
+        if (stage == 2)
+        {
+            boxy.offset = new Vector2(0.02191818f, -0.3f);
+            boxy.size = new Vector2(2.344175f, 2.5f);
+            boxy2.offset = new Vector2(0.03646278f, 1.4f);
+            boxy2.size = new Vector2(2.458501f, 0.7449135f);
+            transform.GetChild(2).GetChild(3).gameObject.SetActive(true);
+            maxPlayerHP += 100;
+        }
+        if (stage == 3)
+        {
+            boxy.offset = new Vector2(0.02191818f, 0f);
+            boxy.size = new Vector2(2.344175f, 3f);
+            boxy2.offset = new Vector2(0.03646278f, 2f);
+            boxy2.size = new Vector2(2.458501f, 0.7449135f);
+            transform.GetChild(2).GetChild(4).gameObject.SetActive(true);
+            maxPlayerHP += 100;
+        }
+        if (stage == 4)
+        {
+            boxy.offset = new Vector2(0.02191818f, -0.5f);
+            boxy.size = new Vector2(2.344175f, 1.9f);
+            boxy2.offset = new Vector2(0.03646278f, 0.9f);
+            boxy2.size = new Vector2(2.458501f, 0.7449135f);
+            transform.GetChild(2).GetChild(5).gameObject.SetActive(true);
+            transform.GetChild(2).GetChild(6).gameObject.SetActive(true);
+            transform.GetChild(2).GetChild(7).gameObject.SetActive(true);
+            maxPlayerHP += 100;
+        }
+    }
+
 }
