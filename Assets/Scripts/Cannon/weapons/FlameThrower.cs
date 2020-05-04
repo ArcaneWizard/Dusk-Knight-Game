8c3be9a0e8bc0d78699763116762e629d370a0cf
Foo Bonjour <osc245@gmail.com>
2020-05-04 10:19:46 +0000

    No Comments

--- Assets/Scripts/Cannon/weapons/FlameThrower.cs
+++ Assets/Scripts/Cannon/weapons/FlameThrower.cs
@@ -21,8 +21,8 @@ public class FlameThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Manage_Sounds m = GameObject.Find("Sound Manager").transform.GetComponent<Manage_Sounds>();
 
-        
 
         if (Input.touchCount > 0)
         {
@@ -34,7 +34,7 @@ public class FlameThrower : MonoBehaviour
             if (playsound)
             {
                 playsound = false;
-                StartCoroutine(Fwoosound());
+                StartCoroutine(Fwoosound(m));
             }
         }
         else
@@ -80,10 +80,10 @@ public class FlameThrower : MonoBehaviour
         flame[7].SetActive(true);
     }
 
-    private IEnumerator Fwoosound()
+    private IEnumerator Fwoosound(Manage_Sounds m)
     {
         Debug.Log("sound playing");
-        Manage_Sounds m = GameObject.Find("Sound Manager").transform.GetComponent<Manage_Sounds>();
+        
         transform.GetComponent<AudioSource>().PlayOneShot(m.flamesound, 0.8f * Manage_Sounds.soundMultiplier);
         yield return new WaitForSeconds(1f);
         playsound = true;
