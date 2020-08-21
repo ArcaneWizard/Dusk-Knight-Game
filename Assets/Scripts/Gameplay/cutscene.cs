8efbac2829dd358d83d40110ded2c1bebecf06a0
Raghav <glacionninja@gmail.com>
2020-08-21 07:32:14 +0000

    Go to the scene named cutscene and hit play. Rez i used: 1200 x 720. don't know about formatting on other rez

---
+++ Assets/Scripts/Gameplay/cutscene.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class cutscene : MonoBehaviour
+{
+    private List<List<Sprite>> cutscenes = new List<List<Sprite>>();
+    public int endScene;
+
+    public static List<int> cutscenesToShow = new List<int>() {0, 1, 2};
+    private int cutscenesShown = 0;
+    private int lastCutSceneShown = 0;
+    private float animationSpeed = 1;
+    private IEnumerator nextCutSceneInst;
+
+    [Space(10)]
+    [Header("Cutscenes")]
+
+    public List<Sprite> kingdomAttacked;
+    public List<Sprite> useRageEffect;
+    public List<Sprite> awakenDuskKnight;
+
+    [Space(10)]
+    [Header("Components")]
+
+    public Image image;
+    public Text caption;
+    private List<string> captions;
+
+    void Start() {
+
+        //add all cutscenes to a list
+        cutscenes.Add(kingdomAttacked);
+        cutscenes.Add(useRageEffect);
+        cutscenes.Add(awakenDuskKnight);
+
+        captions = new List<string>(new string[cutscenes.Count]);
+
+        //add all captions to a list
+        captions[0] = "5 years ago, the Overlord of the Hills tried to siege our kingdom and failed. But in that battle, we were forced to let an artifact of great darkness take over ourselves.";
+        captions[1] = "You were the most affected by the artifact but you could not control your rage to be useful in battle. Now, the war has begun once again and backed by newly formed alliances.";
+        captions[2] = "It’s time for you to learn how to control your dark powers. Harness your rage and get ready to join the fight.";
+
+        //display the first animated cutscene and caption
+        StartCoroutine(showAnimatedCutscene());
+        caption.text = captions[cutscenesToShow[cutscenesShown]];
+    }
+
+    //Animate a given cutscene
+    private IEnumerator showAnimatedCutscene() 
+    {
+        //loop through all the cutscene's sprites with a slight delay btwn each
+        foreach (Sprite scene in cutscenes[cutscenesToShow[cutscenesShown]]) {
+            if (lastCutSceneShown == cutscenesShown)  {
+                yield return new WaitForSeconds(0.1f / animationSpeed);
+                image.sprite = scene;
+            }       
+            else
+                 yield return new WaitForSeconds(0.001f);
+        }
+
+        lastCutSceneShown = cutscenesShown;
+        StartCoroutine(showAnimatedCutscene());
+    }
+
+    //Cycle through diff cutscenes with the NEXT button
+    public void nextCutScene() 
+    {
+        //cycle to the next specified cutscene
+        if (cutscenesShown < cutscenesToShow.Count - 1) {
+            cutscenesShown++;
+            caption.text = captions[cutscenesToShow[cutscenesShown]];
+        }
+
+        //afterwards load the right lvl or lvl selection
+        else         
+            SceneManager.LoadScene(endScene);
+    }
+}
